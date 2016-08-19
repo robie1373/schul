503866e2feae6efdb54e27edfd3f0afbc20c8e20
Robie <robie1373@gmail.com>
2016-08-19 15:10:48 +0000

    attribution

--- python/schul.py
+++ python/schul.py
@@ -1,6 +1,12 @@
 import math
 import numpy as np
 
+##############
+# From or based on:
+# the book _Python Playground Geeky project for the curious
+# programmer_ by Mahesh Venkitachalam. Published by no starch press
+#############
+
 width, height = 640, 480
 
 pos = [width/2.0, height/2.0] + 10*np.random.rand(2*N).reshape(N,2)
